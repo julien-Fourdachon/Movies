fcf9f2f0a2836ac455c719b472a948e7748311d9
Julien Fourdachon <jfourdachon@oph74.fr>
2018-06-05 16:32:40 +0200

    appli movies avec flastList

---
+++ API/TMDBApi.js
@@ -0,0 +1,16 @@
+//API/TMDBApi.js
+
+const API_TOKEN = "e002619bc7e0cd9f310ab06e83640fa4";
+
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + "e002619bc7e0cd9f310ab06e83640fa4" + '&language=fr&query=' + text;
+        return fetch(url)
+            .then((response) => response.json())
+            .catch((error) => {
+            console.error(error);
+            });
+
+}
+export function getImageFromApi(name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
--- components/Search.js
+++ components/Search.js
@@ -1,32 +1,98 @@
+//components/Search.js
 //import de la librairie react
 import React from 'react'
 //import des librairies pour utiliser les components dont on a besoin
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+//import du fichiers contenant les datas qu'on veut afficher (ici via la FlatList)
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' // import { } from ... car c'est un export nommé dans TMDBApi.js
 
 
 class Search extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            films: [],               // l'Api renvoi les films dans un tableau
+            isLoading: false        // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+        }
+    }
+
+
+    //l'underscore "indique" que la méthode est privée
+    _loadFilms() {
+        if (this.searchedText.length > 0) {                                             // Seulement si le texte recherché n'est pas vide
+            this.setState({isLoading : true})
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {          //lancement du chargement
+                this.setState({ films: data.results,                                  //le tableau de données renvoyées par l'api s'appelle résults
+                isLoading : false})                                                  //Arret du chargement
+            })
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.searchedText = text        // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+    }
+
+    _displayLoading() {
+        if(this.stateisLoading){
+            return (
+                <View style = {styles.loading_container}>
+                    <ActivityIndicator size = "large"/>      // choix entre large et small(par défaut)
+                </View>
+            )
+        }
+    }
+
   //la classe React.Component implémente une méthode render(return)
 //  on doit obligatoirement ré-implémenter cet méthode et retourner
 //  les éléments graphiques
-  render(){
-    return(
-      //Ici on rend à l'écran les éléments graphiques de notre component custom
-      <View style = {{ marginTop: 20}}>
-        <TextInput style = {styles.TextInput} placeholder = "Titre du film"/>
-        <Button style = {{ height: 50}} title = "Rechercher" onPress={() => {}}/>
-      </View>
-    )
-  }
+    render() {
+        console.log(this.state.isLoading);
+        return (
+            <View style={styles.main_container}>
+                <TextInput style={styles.textinput}
+                           placeholder='Titre du film'
+                           onChangeText={(text) => this._searchTextInputChanged(text)}
+                           onSubmitEditing={() => this._loadFilms()} />
+                <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._loadFilms()}/>
+                //On utilise FlatList pour afficher une liste de données
+                <FlatList
+                    //on récupère les films du fichier filmsData
+                    data={this.state.films}
+                    keyExtractor={(item) => item.id.toString()}
+                    //on définit notre prop qu'on va passer dans le fichier FilmItem
+                    renderItem={({item}) => <FilmItem film={item}/>}
+                />
+                {this._displayLoading()}
+            </View>
+        )
+    }
 }
 
-const styles = {
-  TextInput: {
-    marginLeft:5,
-    marginRight: 5,
-    height: 50,
-    borderColor: '#000000',
-    borderWidth:1,
-    paddingLeft: 5
-  }
-}
+const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+        marginTop: 20
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    },
+
+    loading_container: {
+        position : 'absolute',
+        left : 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
 export default Search
