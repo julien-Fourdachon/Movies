4bbb722517d649a8624cb922a5eab4b08d720d33
Julien Fourdachon <jfourdachon@oph74.fr>
2018-06-06 16:56:34 +0200

    ajout TabNavBar et logo en bas d'écran

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,8 +1,11 @@
 //Navigation/Navigation.js
-
-import{ createStackNavigator } from 'react-navigation'
+import React from 'react'
+import{ createStackNavigator, createBottomTabNavigator } from 'react-navigation'
 import  Search  from "../components/Search"
-import FilmDetail from "../components/FilmDetail";
+import FilmDetail from "../components/FilmDetail"
+import Favorites from "../components/Favorites"
+import { StyleSheet, Image } from 'react-native';
+
 
     const SearchStackNavigator = createStackNavigator({
         Search:{
@@ -13,7 +16,50 @@ import FilmDetail from "../components/FilmDetail";
         },
         FilmDetail :{
             screen : FilmDetail
+
         }
     })
 
-    export default SearchStackNavigator
+
+const MoviesTabNavigator = createBottomTabNavigator(
+    {
+        Search: {
+            screen: SearchStackNavigator,
+            navigationOptions: {
+                tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+                    return <Image
+                        source={require('../Images/ic_search.png')}
+                        style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+                }
+            }
+        },
+        Favorites: {
+            screen: Favorites,
+            navigationOptions: {
+                tabBarIcon: () => {
+                    return <Image
+                        source={require('../Images/ic_favorite.png')}
+                        style={styles.icon}/>
+                }
+            }
+        }
+    },
+    {
+        tabBarPosition: 'bottom',
+        tabBarOptions: {
+            activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+            inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+            showLabel: false, // On masque les titres
+            showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+        }
+    }
+)
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30
+    }
+})
+
+export default MoviesTabNavigator
